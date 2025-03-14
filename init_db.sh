2762c0ab484ea25788e090ebbdbd9da5c29de6c3
dysfunctioning <chrisismydevemail@gmail.com>
2025-03-14 17:08:16 -0400

    🧹 Cleans up README + add instructions

--- init_db.sh
+++ init_db.sh
@@ -7,10 +7,4 @@ if [ -f /.dockerenv ]; then
     else
         echo "$POSTGRES_DB doesnt exist"
     fi
-    # echo "Database does not exist, creating 'compose_flask_boilerplate'"
-    # psql -v ON_ERROR_STOP=1
-    #     CREATE DATABASE compose_flask_boilerplate;
-    #     CREATE USER root WITH ENCRYPTED PASSWORD 'boilerplate1234';
-    #     GRANT ALL PRIVILEGES ON DATABASE root TO compose_flask_boilerplate;
-    # EOSQL
 fi
