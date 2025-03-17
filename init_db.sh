7766e5fc90567707f168199d05a6351d5a4f3575
dysfunctioning <chrisismydevemail@gmail.com>
2025-03-17 08:39:17 -0400

    💂‍♂️ Fix DB initializer on cold start

--- init_db.sh
+++ init_db.sh
@@ -6,5 +6,11 @@ if [ -f /.dockerenv ]; then
         echo "$POSTGRES_DB DB exists"
     else
         echo "$POSTGRES_DB doesnt exist"
+        echo "Database does not exist, creating '$POSTGRES_DB'"
+        psql -v ON_ERROR_STOP=1
+            CREATE DATABASE $POSTGRES_DB;
+            CREATE USER root WITH ENCRYPTED PASSWORD $POSTGRES_PASSWORD;
+            GRANT ALL PRIVILEGES ON DATABASE $POSTGRES_USER TO $POSTGRES_DB;
+        EOSQL
     fi
 fi
